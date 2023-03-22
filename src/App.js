eebacf08a86868aa13e991783bdf0ee19eca045e
VitoNez5198 <113071024+VitoNez5198@users.noreply.github.com>
2023-03-22 00:57:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import context from "./context";
+import Navegacion from "./components/Navbar";
+import Home from "./views/Home";
+import Pokemones from "./views/Pokemones";
+import Detalle from "./components/Detalle";
 
 function App() {
+  const [pokemones, setPokemones] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <context.Provider value={{ pokemones, setPokemones }}>
+      <Router>
+        <Navegacion />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/pokemones" element={<Pokemones />} />
+          <Route path="/pokemones/:nombre" element={<Detalle />} />
+        </Routes>
+      </Router>
+    </context.Provider>
   );
 }
 
